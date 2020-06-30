62b969ad90d9fe23671473ff810ae913730cf32b
Caboose19 <48289097+Caboose19@users.noreply.github.com>
2020-06-30 11:55:16 -0600

    Drawing a static image to be viewed plus the addition of the engine and its sub class math inqueries

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,7 +1,11 @@
 #include "core.h"
-
+#include "Math/Math.h"
+#include "Random.h"
+#include "Vector2.h"
 #include <iostream>
 
+nc::Vector2 position{ 400,300 };
+std::vector<nc::Vector2> points;
 
 bool Update(float dt)
 {
@@ -12,10 +16,24 @@ void Draw(Core::Graphics& graphics)
 {
 	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
 	graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+
+	for (size_t i = 0; i < 40 - 1; i++)
+	{
+		nc::Vector2 p1 = points[i];
+		nc::Vector2 p2 = points[i + 1];
+
+		graphics.DrawLine(p1.x, p1.y, p2.x, p2.y);
+	}
+
 }
 
 int main()
 {
+	for (size_t i = 0; i < 40; i++) {
+		points.push_back(nc::Vector2{ nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f) });
+
+	}
+
 	char name[] = "CSC196";
 	Core::Init(name, 800, 600);
 	Core::RegisterUpdateFn(Update);
